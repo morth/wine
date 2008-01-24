e22e9e8af27f6eaacdbee2ffaa39395082d8dd3e
Pelle Johansson <morth@morth.org>
2008-01-24 17:20:39 +0100

    TEMP for some reason ext won't be found, so just hard code it.

--- dlls/winex11.drv/opengl.c
+++ dlls/winex11.drv/opengl.c
@@ -480,7 +480,7 @@ LOAD_FUNCPTR(glXFreeMemoryNV)
         pglXGetFBConfigAttrib = pglXGetProcAddressARB((const GLubyte *) "glXGetFBConfigAttrib");
         pglXGetVisualFromFBConfig = pglXGetProcAddressARB((const GLubyte *) "glXGetVisualFromFBConfig");
         pglXQueryDrawable = pglXGetProcAddressARB((const GLubyte *) "glXQueryDrawable");
-    } else if(glxRequireExtension("GLX_SGIX_fbconfig")) {
+    } else if(1 || glxRequireExtension("GLX_SGIX_fbconfig")) {
         pglXChooseFBConfig = pglXGetProcAddressARB((const GLubyte *) "glXChooseFBConfigSGIX");
         pglXGetFBConfigAttrib = pglXGetProcAddressARB((const GLubyte *) "glXGetFBConfigAttribSGIX");
         pglXGetVisualFromFBConfig = pglXGetProcAddressARB((const GLubyte *) "glXGetVisualFromFBConfigSGIX");
